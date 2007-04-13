102653c03666b6cdf70c3bbed86175be754d71c8
Sean Gillies <sean.gillies@gmail.com>
2007-04-13 19:53:55 +0000

    Change copyright from AWMC, update project URL

--- rtree/wrapper.h
+++ rtree/wrapper.h
@@ -1,6 +1,6 @@
 /*
 # =============================================================================
-# Rtree spatial index. Copyright (C) 2006 Ancient World Mapping Center
+# Rtree spatial index. Copyright (C) 2007 Sean C. Gillies
 #
 # This library is free software; you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free
