11bc4add062a3c3a3ee28606dc869230420f0a72
Howard Butler <hobu.inc@gmail.com>
2011-12-29 08:38:03 -0600

    decrufting

--- rtree/__init__.py
+++ rtree/__init__.py
@@ -1,10 +1,3 @@
-# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
-# try:
-#     __import__('pkg_resources').declare_namespace(__name__)
-# except ImportError:
-#     from pkgutil import extend_path
-#     __path__ = extend_path(__path__, __name__)
-
 from index import Rtree
 
 from core import rt
