ce888d1de1ea0e50427e53eedfc0a6e65426f2a1
Sean Gillies <sean@mapbox.com>
2014-07-17 13:50:25 -0600

    Skip custom storage tests for libspatialindex < 1.8.

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -4,16 +4,24 @@ import glob
 import os
 
 #from zope.testing import doctest
+from rtree.index import __c_api_version__
 from .data import boxes15, boxes3, points
 
 optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.NORMALIZE_WHITESPACE |
                doctest.ELLIPSIS)
 
+sindex_version = tuple(map(int, 
+    __c_api_version__.decode('utf-8').split('.')))
+
 def list_doctests():
+    # Skip the custom storage test unless we have libspatialindex 1.8+.
     return [filename
             for filename
-            in glob.glob(os.path.join(os.path.dirname(__file__), '*.txt'))]
+            in glob.glob(os.path.join(os.path.dirname(__file__), '*.txt'))
+            if not (
+                filename.endswith('customStorage.txt') 
+                and sindex_version < (1,8,0))]
 
 def open_file(filename, mode='r'):
     """Helper function to open files from within the tests package."""
@@ -34,6 +42,8 @@ def test_suite():
                               setUp=setUp)
          for filename
          in sorted(list_doctests())])
+
+print(list_doctests())
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
