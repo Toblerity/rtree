d9807b7ae42ba78e3698d7a54e0f66cfacfcdf2d
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-03-05 03:01:00 -0500

    TST: Copy test data to temporary working directory.
    This way, there's no need to have a special imported file to get
    __file__ in the doctest.

--- tests/data.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-boxes15 = []
-f = open(os.path.join(os.path.dirname(__file__), 'boxes_15x15.data'), 'r')
-for line in f.readlines():
-    if not line:
-        break
-    [left, bottom, right, top] = [float(x) for x in line.split()]
-    boxes15.append((left, bottom, right, top))
