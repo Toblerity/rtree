6b973d49835b9ee4a28d245155e8b0f7ed252009
Leonard Buskin <leonardbuskin@gmail.com>
2019-02-11 18:24:09 -0500

    Adds Index_Contains_obj/id methods

--- rtree/core.py
+++ rtree/core.py
@@ -206,6 +206,16 @@ rt.Index_Intersects_obj.argtypes = [ctypes.c_void_p,
 rt.Index_Intersects_obj.restype = ctypes.c_int
 rt.Index_Intersects_obj.errcheck = check_return
 
+rt.Index_Contains_obj.argtypes = [ctypes.c_void_p,
+                                  ctypes.POINTER(ctypes.c_double),
+                                  ctypes.POINTER(ctypes.c_double),
+                                  ctypes.c_uint32,
+                                  ctypes.POINTER(
+                                      ctypes.POINTER(ctypes.c_void_p)),
+                                  ctypes.POINTER(ctypes.c_uint64)]
+rt.Index_Contains_obj.restype = ctypes.c_int
+rt.Index_Contains_obj.errcheck = check_return
+
 rt.Index_Intersects_id.argtypes = [ctypes.c_void_p,
                                    ctypes.POINTER(ctypes.c_double),
                                    ctypes.POINTER(ctypes.c_double),
@@ -215,6 +225,16 @@ rt.Index_Intersects_id.argtypes = [ctypes.c_void_p,
                                    ctypes.POINTER(ctypes.c_uint64)]
 rt.Index_Intersects_id.restype = ctypes.c_int
 rt.Index_Intersects_id.errcheck = check_return
+
+rt.Index_Contains_id.argtypes = [ctypes.c_void_p,
+                                   ctypes.POINTER(ctypes.c_double),
+                                   ctypes.POINTER(ctypes.c_double),
+                                   ctypes.c_uint32,
+                                   ctypes.POINTER(
+                                       ctypes.POINTER(ctypes.c_int64)),
+                                   ctypes.POINTER(ctypes.c_uint64)]
+rt.Index_Contains_id.restype = ctypes.c_int
+rt.Index_Contains_id.errcheck = check_return
 
 rt.Index_Intersects_count.argtypes = [ctypes.c_void_p,
                                       ctypes.POINTER(ctypes.c_double),
