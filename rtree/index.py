7efba6796cc7e771ee260a9c8ef963a31606eb25
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2019-10-28 07:32:52 +0000

    Resolve issue with referencing objects in RtreeContainer

--- rtree/index.py
+++ rtree/index.py
@@ -1518,9 +1518,9 @@ class RtreeContainer(Rtree):
             [34.37768294..., 26.73758537..., 49.37768294..., 41.73758537...]
         """
         if args:
-            if isinstance(args[0], rtree.index.string_types) \
+            if isinstance(args[0], string_types) \
                     or isinstance(args[0], bytes) \
-                    or isinstance(args[0], rtree.index.ICustomStorage):
+                    or isinstance(args[0], ICustomStorage):
                 raise ValueError('%s supports only in-memory indexes'
                                  % self.__class__)
         self._objects = {}
@@ -1550,7 +1550,7 @@ class RtreeContainer(Rtree):
 
         """
         try:
-            count = self._objects[id(obj)] + 1
+            count = self._objects[id(obj)][0] + 1
         except KeyError:
             count = 1
         self._objects[id(obj)] = (count, obj)
@@ -1678,14 +1678,14 @@ class RtreeContainer(Rtree):
 
         """
         try:
-            count = self._objects[id(obj)] - 1
+            count = self._objects[id(obj)][0] - 1
         except KeyError:
             raise IndexError('object is not in the index')
         if count == 0:
-            del self._objects[obj]
+            del self._objects[id(obj)]
         else:
             self._objects[id(obj)] = (count, obj)
-        return super(RtreeContainer, self).delete(id, coordinates)
+        return super(RtreeContainer, self).delete(id(obj), coordinates)
 
     def leaves(self):
         return [(self._objects[id][1], [self._objects[child_id][1]
