ce6200a882260d0df2d02f598c8df5338cf6d75b
Howard Butler <howard@hobu.co>
2019-11-13 23:38:38 -0600

    guard correct methods

--- rtree/core.py
+++ rtree/core.py
@@ -184,16 +184,15 @@ rt.Index_Intersects_obj.restype = ctypes.c_int
 rt.Index_Intersects_obj.errcheck = check_return
 
 
-
-rt.Index_Contains_id.argtypes = [ctypes.c_void_p,
+rt.Index_Intersects_id.argtypes = [ctypes.c_void_p,
                                    ctypes.POINTER(ctypes.c_double),
                                    ctypes.POINTER(ctypes.c_double),
                                    ctypes.c_uint32,
                                    ctypes.POINTER(
                                        ctypes.POINTER(ctypes.c_int64)),
                                    ctypes.POINTER(ctypes.c_uint64)]
-rt.Index_Contains_id.restype = ctypes.c_int
-rt.Index_Contains_id.errcheck = check_return
+rt.Index_Intersects_id.restype = ctypes.c_int
+rt.Index_Intersects_id.errcheck = check_return
 
 rt.Index_Intersects_count.argtypes = [ctypes.c_void_p,
                                       ctypes.POINTER(ctypes.c_double),
@@ -302,15 +301,16 @@ try:
     rt.Index_Contains_obj.restype = ctypes.c_int
     rt.Index_Contains_obj.errcheck = check_return
 
-    rt.Index_Intersects_id.argtypes = [ctypes.c_void_p,
+    rt.Index_Contains_id.argtypes = [ctypes.c_void_p,
                                        ctypes.POINTER(ctypes.c_double),
                                        ctypes.POINTER(ctypes.c_double),
                                        ctypes.c_uint32,
                                        ctypes.POINTER(
                                            ctypes.POINTER(ctypes.c_int64)),
                                        ctypes.POINTER(ctypes.c_uint64)]
-    rt.Index_Intersects_id.restype = ctypes.c_int
-    rt.Index_Intersects_id.errcheck = check_return
+    rt.Index_Contains_id.restype = ctypes.c_int
+    rt.Index_Contains_id.errcheck = check_return
+
 except AttributeError:
     pass
 
