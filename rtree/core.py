c47c39c12d95ff1bf55086179af43f9e9f09600d
Howard Butler <howard@hobu.co>
2019-11-25 07:36:05 -0600

    use ctypes.utils.find_libary for posix library location #120

--- rtree/core.py
+++ rtree/core.py
@@ -95,12 +95,8 @@ if os.name == 'nt':
 elif os.name == 'posix':
     if 'conda' in sys.version:
         os.environ['PATH'] = "{};{}".format(os.environ['PATH'], os.path.join(sys.prefix, "lib"))
-    if 'linux' in sys.platform:
-        lib_name = 'libspatialindex_c.so'
-    elif 'darwin' in sys.platform:
-        lib_name = 'libspatialindex_c.dylib'
-    else:
-        lib_name = 'libspatialindex_c'
+    
+    lib_name = find_library('spatialindex_c')
     rt = ctypes.CDLL(lib_name)
 else:
     raise RTreeError('Unsupported OS "%s"' % os.name)
