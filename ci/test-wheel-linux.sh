9edef068be6b2a6be30ec7b381b691dd62af1b4e
Howard Butler <howard@hobu.co>
2020-04-20 09:55:15 -0500

    wheel testing

--- ci/test-wheel-linux.sh
+++ ci/test-wheel-linux.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-PYTHONROOT="/opt/python/$1"
 
 
-$PYTHONROOT/bin/python -m pip install pytest numpy
+python -m pip install pytest numpy
 
 for f in dist/*.whl
 do
 
-    $PYTHONROOT/bin/python -m pip install $f
+    python -m pip install $f
 done;
 
 pytest
