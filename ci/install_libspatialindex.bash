73706f18398542e55285ff4142c13c1bb196a9d4
mikedh <mik3dh@gmail.com>
2020-04-24 19:26:00 -0400

    first pass

---
+++ ci/install_libspatialindex.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -xe
+
+# A simple script to install libspatialindex from a Github Release
+VERSION=1.9.3
+SHA256=63a03bfb26aa65cf0159f925f6c3491b6ef79bc0e3db5a631d96772d6541187e
+
+
+rm $VERSION.zip | true
+wget https://github.com/libspatialindex/libspatialindex/archive/$VERSION.zip
+
+# check the file hash
+echo "${SHA256} ${VERSION}.zip" | sha256sum --check
+
+rm -rf "libspatialindex-${VERSION}" | true
+unzip $VERSION
+cd libspatialindex-${VERSION}
+
+mkdir build
+cd build
+cmake ..
+make -j 4
+make install
+
+cd ../..
+rm -rf "libspatialindex-${VERSION}"
