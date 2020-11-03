8170e30d0c23457846c99dd64764285c17a4fe99
Mike Dawson-Haggerty <mikedh@kerfed.com>
2020-11-03 11:37:13 -0500

    back

--- ci/install_libspatialindex.bat
+++ ci/install_libspatialindex.bat
@@ -3,12 +3,12 @@ python -c "import sys; print(sys.version)"
 // A simple script to install libspatialindex from a Github Release
 curl -L -O https://github.com/libspatialindex/libspatialindex/archive/1.9.3.zip
 
+
 unzip 1.9.3.zip
 cd libspatialindex-1.9.3
 
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make
-make install
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+ninja
