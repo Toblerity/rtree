fd1bc9d2d50100f5a5e79fa57b8aa381fb7ba5d4
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-11-03 16:36:37 -0500

    windows all use choco

--- ci/install_libspatialindex.bat
+++ ci/install_libspatialindex.bat
@@ -1,7 +1,5 @@
 python -c "import sys; print(sys.version)"
 
-pip install cmake ninja
-
 // A simple script to install libspatialindex from a Github Release
 curl -L -O https://github.com/libspatialindex/libspatialindex/archive/1.9.3.zip
 
