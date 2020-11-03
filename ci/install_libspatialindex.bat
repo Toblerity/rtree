dad8987326b7628278b38aece7a35d1a27a3c5e0
Mike Dawson-Haggerty <mikedh@kerfed.com>
2020-11-03 12:13:31 -0500

    remove more

--- ci/install_libspatialindex.bat
+++ ci/install_libspatialindex.bat
@@ -9,11 +9,6 @@ curl -L -O https://github.com/libspatialindex/libspatialindex/archive/1.9.3.zip
 unzip 1.9.3.zip
 cd libspatialindex-1.9.3
 
-mkdir build
-cd build
-
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release .
 make
-make insta
-
-// ninja
+make install
