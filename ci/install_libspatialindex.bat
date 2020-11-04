703047358e3d2fdbd71629cc39f41dae133992cc
Mike Dawson-Haggerty <mikedh@kerfed.com>
2020-11-04 16:40:40 -0500

    set env in gh

--- ci/install_libspatialindex.bat
+++ ci/install_libspatialindex.bat
@@ -3,17 +3,13 @@ python -c "import sys; print(sys.version)"
 // A simple script to install libspatialindex from a Github Release
 curl -L -O https://github.com/libspatialindex/libspatialindex/archive/1.9.3.zip
 
-
 unzip 1.9.3.zip
 cd libspatialindex-1.9.3
 
 mkdir build
 cd build
 
-set CC=C:\msys64\mingw64\bin\clang.exe
-set CXX=C:\msys64\mingw64\bin\clang.exe
-
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -D CMAKE_BUILD_TYPE=Release ..
 ninja
 
 
