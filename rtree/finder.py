a3e67f0d85d4099e3f3f9e411b60f8417bab4265
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-11-11 10:53:58 -0500

    windows finder logic

--- rtree/finder.py
+++ rtree/finder.py
@@ -11,67 +11,62 @@ import ctypes
 
 from ctypes.util import find_library
 
+# the current working directory of this file
+_cwd = os.path.abspath(os.path.expanduser(
+    os.path.dirname(__file__)))
+
 
 def load(return_path=False):
 
     full_path, lib_path, lib_name = None, None, None
     if os.name == 'nt':
-
         def _load_library(dllname, loadfunction, dllpaths=('', )):
             """Load a DLL via ctypes load function. Return None on failure.
             Try loading the DLL from the current package directory first,
             then from the Windows DLL search path.
             """
-            try:
-                dllpaths = (os.path.abspath(os.path.dirname(__file__)),
-                            ) + dllpaths
-            except NameError:
-                # no __file__ attribute on PyPy and some frozen distributions
-                pass
             for path in dllpaths:
-                if path:
-                    # temporarily add the path to the PATH environment variable
-                    # so Windows can find additional DLL dependencies.
-                    try:
-                        oldenv = os.environ['PATH']
-                        os.environ['PATH'] = path + ';' + oldenv
-                    except KeyError:
-                        oldenv = None
+                if not path or not os.path.exists(path):
+                    continue
+                # temporarily add the path to the PATH environment variable
+                # so Windows can find additional DLL dependencies.
                 try:
-                    return loadfunction(os.path.join(path, dllname))
+                    oldenv = os.environ['PATH']
+                    os.environ['PATH'] = path + ';' + oldenv
+                except KeyError:
+                    oldenv = None
+                try:
+                    return loadfunction(os.path.join(
+                        path, dllname)), os.path.join(path, dllname)
                 except (WindowsError, OSError):
                     pass
                 finally:
                     if path and oldenv is not None:
                         os.environ['PATH'] = oldenv
-            return None
+            return None, None
 
-        base_name = 'spatialindex_c'
         if '64' in platform.architecture()[0]:
-            arch = '64'
+            archs = ('64', '32')
         else:
-            arch = '32'
+            archs = ('32', '64')
 
-        lib_name = '%s-%s.dll' % (base_name, arch)
         rt = None
-        if 'SPATIALINDEX_C_LIBRARY' in os.environ:
-            full_path = os.environ['SPATIALINDEX_C_LIBRARY']
-            lib_path, lib_name = os.path.split(full_path)
-            rt = _load_library(lib_name, ctypes.cdll.LoadLibrary, (lib_path,))
-        # try wheel location
+        for arch in arches:
+            lib_name = 'spatialindex_c-{}.dll'.format(base_name, arch)
+            # generate a bunch of candidate locations where the
+            # libspatialindex DLL *might* be hanging out
+            candidates = [os.environ.get('SPATIALINDEX_C_LIBRARY', None),
+                          _cwd,
+                          os.path.join(_cwd, 'lib'),
+                          os.path.join(sys.prefix, "Library", "bin")]
+            # run through our list of candidate locations
+            rt, full_path = _load_library(
+                lib_name, ctypes.cdll.LoadLibrary, candidates)
+            if rt is not None:
+                break
+
         if not rt:
-            lib_path = os.path.abspath(os.path.join(
-                os.path.dirname(__file__), "lib"))
-            full_path = os.path.join(lib_path, lib_name)
-            rt = _load_library(lib_name, ctypes.cdll.LoadLibrary, (lib_path,))
-        # try conda location
-        if not rt:
-            if 'conda' in sys.version:
-                lib_path = os.path.join(sys.prefix, "Library", "bin")
-                rt = _load_library(
-                    lib_name, ctypes.cdll.LoadLibrary, (lib_path,))
-                full_path = os.path.join(lib_path, lib_name)
-        if not rt:
+            # try a bare call for funsies
             rt = _load_library(lib_name, ctypes.cdll.LoadLibrary)
 
         if not rt:
