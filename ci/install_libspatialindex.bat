963e9d564dc307607c4f7bc0a5350a93188ad122
Howard Butler <howard@hobu.co>
2020-12-22 22:35:10 -0600

    use ninja to build libspatialindex in release mode on win

--- ci/install_libspatialindex.bat
+++ ci/install_libspatialindex.bat
@@ -1,23 +1,29 @@
 python -c "import sys; print(sys.version)"
 
-// A simple script to install libspatialindex from a Github Release
-curl -L -O https://github.com/libspatialindex/libspatialindex/archive/1.9.3.zip
+REM A simple script to install libspatialindex from a Github Release
+REM curl -L https://github.com/libspatialindex/libspatialindex/archive/1.9.3.zip  | python -c "import zipfile,sys,io;data=io.StringIO(sys.stdin.read());zipfile.ZipFile(data).extractall('.')" 
+set SIDX_VERSION=1.9.3
 
-unzip 1.9.3.zip
-copy %~dp0\CMakeLists.txt libspatialindex-1.9.3\CMakeLists.txt
-cd libspatialindex-1.9.3
+curl -OL "https://github.com/libspatialindex/libspatialindex/archive/%SIDX_VERSION%.zip" 
+
+tar xvf "%SIDX_VERSION%.zip"
+
+REM unzip 1.9.3.zip
+REM copy %~dp0\CMakeLists.txt libspatialindex-1.9.3\CMakeLists.txt
+cd libspatialindex-%SIDX_VERSION%
 
 mkdir build
 cd build
 
-cmake -D CMAKE_BUILD_TYPE=Release ..
+cmake -D CMAKE_BUILD_TYPE=Release -G Ninja ..
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64\MSBuild.exe" spatialindex.sln
+ninja
 
 mkdir %~dp0\..\rtree\lib
-copy bin\Debug\*.dll %~dp0\..\rtree\lib
+copy bin\*.dll %~dp0\..\rtree\lib
 rmdir /Q /S bin
 
 dir %~dp0\..\rtree\
 dir %~dp0\..\rtree\lib
+
 
