e77ebf71a3fbd277eaf699387c2eaa3dae335ca6
Howard Butler <howard@hobu.co>
2020-12-23 11:49:05 -0600

    use ninja to build libspatialindex in release mode on win (#176)
    * use ninja to build libspatialindex in release mode on win
    
    * we need pip ninja
    
    * setup msvc on win

--- ci/install_libspatialindex.bat
+++ ci/install_libspatialindex.bat
@@ -1,23 +1,29 @@
 python -c "import sys; print(sys.version)"
 
-// A simple script to install libspatialindex from a Github Release
-curl -L -O https://github.com/libspatialindex/libspatialindex/archive/1.9.3.zip
+set SIDX_VERSION=1.9.3
 
-unzip 1.9.3.zip
-copy %~dp0\CMakeLists.txt libspatialindex-1.9.3\CMakeLists.txt
-cd libspatialindex-1.9.3
+curl -OL "https://github.com/libspatialindex/libspatialindex/archive/%SIDX_VERSION%.zip" 
+
+tar xvf "%SIDX_VERSION%.zip"
+
+REM unzip 1.9.3.zip
+REM copy %~dp0\CMakeLists.txt libspatialindex-1.9.3\CMakeLists.txt
+cd libspatialindex-%SIDX_VERSION%
 
 mkdir build
 cd build
 
-cmake -D CMAKE_BUILD_TYPE=Release ..
+pip install ninja
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64\MSBuild.exe" spatialindex.sln
+cmake -D CMAKE_BUILD_TYPE=Release -G Ninja ..
+
+ninja
 
 mkdir %~dp0\..\rtree\lib
-copy bin\Debug\*.dll %~dp0\..\rtree\lib
+copy bin\*.dll %~dp0\..\rtree\lib
 rmdir /Q /S bin
 
 dir %~dp0\..\rtree\
 dir %~dp0\..\rtree\lib
+
 
