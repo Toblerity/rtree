fde4aaaea3cb1d8cb393ab81cda3f124a41806dd
Mike Taves <mwtoews@gmail.com>
2023-08-21 10:06:29 +1200

    Fix finder if "rtree" is not installed

--- rtree/finder.py
+++ rtree/finder.py
@@ -79,14 +79,18 @@ def load() -> ctypes.CDLL:
             lib_name = "libspatialindex_c.so"
 
             # add path for binary wheel prepared with cibuildwheel/auditwheel
-            for file in importlib.metadata.files("rtree"):  # type: ignore
-                if (
-                    file.parent.name == "Rtree.libs"
-                    and file.stem.startswith("libspatialindex")
-                    and ".so" in file.suffixes
-                ):
-                    _candidates.insert(1, os.path.join(str(file.locate())))
-                    break
+            try:
+                pkg_files = importlib.metadata.files("rtree")
+                for file in pkg_files:  # type: ignore
+                    if (
+                        file.parent.name == "Rtree.libs"
+                        and file.stem.startswith("libspatialindex")
+                        and ".so" in file.suffixes
+                    ):
+                        _candidates.insert(1, os.path.join(str(file.locate())))
+                        break
+            except importlib.metadata.PackageNotFoundError:
+                pass
 
         # get the starting working directory
         cwd = os.getcwd()
