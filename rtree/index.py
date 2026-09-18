4e215560ee202e4b9804b86ebe76d6d1fe44ce3b
Adam J. Stewart <ajstewart426@gmail.com>
2026-09-18 14:41:28 +0200

    Remove old error msg
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- rtree/index.py
+++ rtree/index.py
@@ -346,7 +346,6 @@ class Index:
         if len(string) < INDEX_JSON_SERIALIZATION_LIMIT_SIZE:
             return json.loads(str(string, "utf-8"))
         else:
-            raise TimeoutError("JSON string is too large to quickly decode")
             raise TimeoutError(f"Unable to load serialized index data. The JSON string is above the serialization limit of {INDEX_JSON_SERIALIZATION_LIMIT_SIZE")
 
     def close(self) -> None:
